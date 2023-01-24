6a6aeb82979f12e8dc87d28e42925212e372cf61
EthanG78 <ethangarnier78@gmail.com>
2023-01-24 15:41:49 -0400

    implemented recalibration feature. if we detect one of the sensors have fallen our of calibration, we fetch the cal offsets from calibration.json and restore them before retaking the reading

--- imu_rpi/imu.py
+++ imu_rpi/imu.py
@@ -14,6 +14,7 @@ bno = BNO055.BNO055(serial_port='/dev/serial0')
 
 CALIBRATION_FILE = "calibration.json"
 
+
 def print_sys_info(logfile):
     """Print the BNO055 IMU system information"""
     # Print system status and self test result.
@@ -23,13 +24,15 @@ def print_sys_info(logfile):
     print(statusStr)
     logfile.write(statusStr + '\n')
 
-    selfTestStr = "Self test result (0x0F is normal): 0x{0:02X}".format(self_test)
+    selfTestStr = "Self test result (0x0F is normal): 0x{0:02X}".format(
+        self_test)
     print(selfTestStr)
     logfile.write(selfTestStr + '\n')
 
     # Print out an error if system status is in error mode.
     if status == 0x01:
-        errorStr = "System error: {0}\nSee datasheet section 4.3.59 for the meaning.".format(error)
+        errorStr = "System error: {0}\nSee datasheet section 4.3.59 for the meaning.".format(
+            error)
         print(errorStr)
         logfile.write(errorStr + '\n')
 
@@ -39,6 +42,7 @@ def print_sys_info(logfile):
         sw, bl, accel, mag, gyro)
     print(revisionStr)
     logfile.write(revisionStr + '\n')
+
 
 def calibrate_imu():
     """Instruct the user on how to manually calibrate
@@ -52,20 +56,24 @@ def calibrate_imu():
 
     print("\n-----------\n")
     print("Calibrating BNO055 Device:\n")
-    
-    print("\tTo calibrate the Gyroscope, place the device down and let rest still...\n") 
-    while gyro != 3: sys, gyro, accel, mag = bno.get_calibration_status()
+
+    print("\tTo calibrate the Gyroscope, place the device down and let rest still...\n")
+    while gyro != 3:
+        sys, gyro, accel, mag = bno.get_calibration_status()
     print("\tDone calibrating Gyroscope\n")
 
     print("\tTo calibrate the Magnetometer, move the device through the air in a figure-8 pattern...\n")
-    while mag != 3: sys, gyro, accel, mag = bno.get_calibration_status()
+    while mag != 3:
+        sys, gyro, accel, mag = bno.get_calibration_status()
     print("\tDone calibrating Magnetometer\n")
 
     print("\tTo calibrate the Accelerometer, rotate the device around an axis in 45 degree increments...\n")
-    while accel != 3: sys, gyro, accel, mag = bno.get_calibration_status()
+    while accel != 3:
+        sys, gyro, accel, mag = bno.get_calibration_status()
     print("\tDone calibrating Accelerometer\n")
 
-    while sys != 3: sys, gyro, accel, mag = bno.get_calibration_status()
+    while sys != 3:
+        sys, gyro, accel, mag = bno.get_calibration_status()
 
     print("Calibration complete!\n")
     print("-----------\n\n")
@@ -82,8 +90,9 @@ def run_imu():
     """
     # Initialize the BNO055 and stop if something went wrong.
     if not bno.begin():
-        raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
-    
+        raise RuntimeError(
+            'Failed to initialize BNO055! Is the sensor connected?')
+
     # Construct the logfile
     now = datetime.now()
     dt = now.strftime("%d-%m-%Y_%H-%M-%S")
@@ -105,6 +114,19 @@ def run_imu():
             data = "Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}".format(
                 heading, roll, pitch, sys, gyro, accel, mag)
 
+            # If at any point we fall out of calibration, lets fetch the calibration
+            # data from calibration.json, reset the offsets of the sensors,
+            # and fetch the data again.
+            if sys < 3 or mag < 3 or accel < 3 or gyro < 3:
+                with open(CALIBRATION_FILE, "r") as calfile:
+                    prevcal = json.load(cal_file)
+                    bno.set_calibration(prevcal)
+
+                heading, roll, pitch = bno.read_euler()
+                sys, gyro, accel, mag = bno.get_calibration_status()
+                data = "Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}".format(
+                    heading, roll, pitch, sys, gyro, accel, mag)
+                    
             # Print data to stdout and log file
             print(data)
             logfile.write(data + '\n')
@@ -129,6 +151,7 @@ def run_imu():
             # Sleep for a second until the next reading.
             time.sleep(1)
 
+
 def parse_args():
     if len(sys.argv) > 2:
         for arg in sys.argv:
@@ -136,6 +159,7 @@ def parse_args():
                 logging.basicConfig(level=logging.DEBUG)
             elif arg.lower() == '-h':
                 print("Usage: sudo python imu.py [-v -h]")
+
 
 if __name__ == '__main__':
     # Parse command line arguments
